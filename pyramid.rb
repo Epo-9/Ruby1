b412133d8eb718cadc159f8ce10bda92c9dc423b
Epo-9 <eponine.paillet@gmail.com>
2019-07-09 23:02:14 +0200

    ajout pyramide

---
+++ pyramid.rb
@@ -0,0 +1,11 @@
+#construction de la pyramide qui est obtenue à l'aide d'un nombre proposé
+#par notre utilisateur
+puts "Bonjour cher utilisateur. Peux-tu m'indiquer un nombre 
+s'il te plaît?"
+user_year_of_birth = gets.chomp.to_i
+
+n = 1
+while n <= user_year_of_birth
+  puts ("# " * n).rjust(60)
+  n += 1
+end
