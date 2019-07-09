7507224bd1f8a9c93be581ec67dca97365f1d6ea
Epo-9 <eponine.paillet@gmail.com>
2019-07-09 22:19:52 +0200

    ajout exo 18

---
+++ exo_18.rb
@@ -0,0 +1,32 @@
+email_List = Array.[]
+
+1.upto(9) do |i|
+
+	#if i < 10
+	#email = "jean.dupont.0#{i}@email.fr"
+	#else
+	email = "jean.dupont.0#{i}@email.fr"
+email_List.push(email)
+
+	email_List.push (email)
+i =i+1
+
+	#end
+
+end
+
+	end
+
+10.upto(50) do |i|	
+
+	email = "jean.dupont.#{i}@email.fr"
+email_List.push(email)
+
+	email_List.push (email)
+i =i+1
+	end
+
+end
+
+puts email_List
+puts email_List 
